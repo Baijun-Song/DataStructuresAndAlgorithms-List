40072e0ad13a7c7e7f68563abbf9515fa46d4b3e
Baijun Song <songbaijunsglyh@outlook.com>
2023-01-07 10:35:52 +0800

    Refactor

---
+++ List/LinkedList/Public/LinkedList+Equatable.swift
@@ -0,0 +1,21 @@
+extension LinkedList: Equatable where Element: Equatable {
+  @inlinable
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    var currentLeftNode = lhs.head
+    var currentRightNode = rhs.head
+    while true {
+      switch (currentLeftNode, currentRightNode) {
+      case let (leftNode?, rightNode?):
+        if leftNode.value != rightNode.value {
+          return false
+        }
+        currentLeftNode = leftNode.next
+        currentRightNode = rightNode.next
+      case (nil, nil):
+        return true
+      default:
+        return false
+      }
+    }
+  }
+}
